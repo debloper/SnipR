01ae6fede5bcc166d0fd54c9faefdca4c974631c
Soumya Deb <debloper@gmail.com>
2011-03-30 18:18:36 +0530

    Mobile interface added, albeit buggy now.

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -5,16 +5,9 @@ var snipr = {
     // initialization code
     this.initialized = true;
     this.strings = document.getElementById("snipr-overlay");
-/*	ipmib = document.getElementById("identity-popup-more-info-button");
-	ipmiHbox = ipmib.parentNode;
-	ipmiSibling = document.createElement("button");
-	ipmiSiblingNode = ipmiHbox.insertBefore(ipmiSibling, ipmib);
-	ipmiSiblingNode.setAttribute("id","identity-popup-snipr-info-button");
-	ipmiSiblingNode.setAttribute("label","SnipR Information");
-	ipmiSiblingNode.setAttribute("oncommand","snipr.onTrigger();");*/
   },
   
-  onTrigger: function() {
+  FxTrigger: function() {
 	var host = gBrowser.contentWindow.location.hostname;
 	var ips = new Array();
 	var DNS = Components.classes['@mozilla.org/network/dns-service;1']
@@ -23,9 +16,20 @@ var snipr = {
 	while (DNS && DNS.hasMore()) { ips.push(DNS.getNextAddrAsString()); }
 	ip = ips[0];
 	ownerTab = gBrowser.selectedTab;
-	//XULBrowserWindow.inContentWhitelist.push("chrome://snipr/content/snipr.xul");
-	//sniprTab = gBrowser.addTab("chrome://snipr/content/snipr.xul?"+host+"#"+ip, {referrerURI:gBrowser.currentURI, owner:ownerTab});
-	sniprTab = gBrowser.addTab("http://google.com/", {referrerURI:gBrowser.currentURI, owner:ownerTab});
+	sniprTab = gBrowser.addTab("chrome://snipr/content/snipr.xul?"+host+"#"+ip, {referrerURI:gBrowser.currentURI, owner:ownerTab});
 	gBrowser.selectedTab = sniprTab;
+	},
+	
+  FnTrigger: function() {
+	var host = getBrowser().currentURI.host;
+	var ips = new Array();
+	var DNS = Components.classes['@mozilla.org/network/dns-service;1']
+				.getService(Components.interfaces.nsIDNSService)
+					.resolve(host, true);
+	while (DNS && DNS.hasMore()) { ips.push(DNS.getNextAddrAsString()); }
+	ip = ips[0];
+	ownerTab = Browser.selectedTab;
+	sniprTab = Browser.addTab("chrome://snipr/content/snipr.xul?"+host+"#"+ip);
+	Browser.selectedTab = sniprTab;
 	}
 };
