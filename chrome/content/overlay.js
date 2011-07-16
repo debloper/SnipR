187b7aeba58b4c12e7359b6dff37513a59500254
Soumya Deb <debloper@gmail.com>
2011-07-16 12:56:17 +0530

    [Feature] Live IP view on Mouse Event + Code-indentation fix

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -2,10 +2,10 @@ window.addEventListener("load", function(event) { snipr.onLoad(event) }, false);
 
 var snipr = {
   onLoad: function(event) {
-    this.initialized = true;
-    this.strings = document.getElementById("snipr-overlay");
+	this.initialized = true;
+	this.strings = document.getElementById("snipr-overlay");
   },
-  
+
   FxTrigger: function() {
 	var host = gBrowser.contentWindow.location.hostname;
 	var ip = snipr.resolveIP(host);
@@ -13,7 +13,7 @@ var snipr = {
 	sniprTab = gBrowser.addTab("http://debloper.github.com/SnipR/firefox.htm?"+host+"#"+ip, {referrerURI:gBrowser.currentURI, owner:ownerTab});
 	gBrowser.selectedTab = sniprTab;
   },
-  
+
   FnTrigger: function() {
 	var host = getBrowser().currentURI.host;
 	var ip = snipr.resolveIP(host);
@@ -21,7 +21,7 @@ var snipr = {
 	sniprTab = Browser.addTab("http://debloper.github.com/SnipR/fennec.htm?"+host+"#"+ip);
 	Browser.selectedTab = sniprTab;
   },
-  
+
   resolveIP: function(host) {
 	var ips = new Array();
 	var DNS = Components.classes['@mozilla.org/network/dns-service;1']
@@ -30,5 +30,13 @@ var snipr = {
 	while (DNS && DNS.hasMore()) { ips.push(DNS.getNextAddrAsString()); }
 	var ip = ips[0];
 	return ip;
+  },
+
+  updateLabel: function() {
+	var label = document.getElementById("snipr-crosshair");
+	if (label.value == "SnipR") {
+		try { label.value = snipr.resolveIP(gBrowser.contentWindow.location.hostname); }
+		catch (e) { label.value = "No Connection or Local Page!" }
+	} else { label.value = "SnipR"; }
   }
 };
