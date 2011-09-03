f58b1dc4b13e01fd69783bbf8cfb13ce14f923ba
Soumya Deb <debloper@gmail.com>
2011-09-03 21:09:20 +0530

    Browser detection; domain-parsing & url-rendering abstraction

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -1,24 +1,38 @@
 window.addEventListener("load", function(event) { snipr.onLoad(event) }, false);
 
 var snipr = {
+  browser: "",
+
   onLoad: function(event) {
 	this.initialized = true;
 	this.strings = document.getElementById("snipr-overlay");
+	if ( gBrowser ) { snipr.browser = "firefox"; }
+	else { snipr.browser = "fennec"; }
+  },
+
+  resolveHost: function() {
+	  if (snipr.browser == "firefox") {
+		  return gBrowser.contentWindow.location.hostname;
+	  } else return getBrowser().currentURI.host;
+  },
+
+  renderUrl: function(host,ip) {
+	  return "http://debloper.github.com/SnipR/"+snipr.browser+".htm?"+host+"#"+ip;
   },
 
   FxTrigger: function() {
-	var host = gBrowser.contentWindow.location.hostname;
+	var host = snipr.resolveHost();
 	var ip = snipr.resolveIP(host);
 	ownerTab = gBrowser.selectedTab;
-	sniprTab = gBrowser.addTab("http://debloper.github.com/SnipR/firefox.htm?"+host+"#"+ip, {referrerURI:gBrowser.currentURI, owner:ownerTab});
+	sniprTab = gBrowser.addTab(snipr.renderUrl(host,ip), {referrerURI:gBrowser.currentURI, owner:ownerTab});
 	gBrowser.selectedTab = sniprTab;
   },
 
   FnTrigger: function() {
-	var host = getBrowser().currentURI.host;
+	var host = snipr.resolveHost();
 	var ip = snipr.resolveIP(host);
 	ownerTab = Browser.selectedTab;
-	sniprTab = Browser.addTab("http://debloper.github.com/SnipR/fennec.htm?"+host+"#"+ip);
+	sniprTab = Browser.addTab(snipr.renderUrl(host,ip));
 	Browser.selectedTab = sniprTab;
   },
 
@@ -35,7 +49,7 @@ var snipr = {
   updateLabel: function() {
 	var label = document.getElementById("snipr-crosshair");
 	if (label.value == "SnipR") {
-		try { label.value = snipr.resolveIP(gBrowser.contentWindow.location.hostname); }
+		try { label.value = snipr.resolveIP(snipr.resolveHost()); }
 		catch (e) { label.value = "No Connection or Local Page!" }
 	} else { label.value = "SnipR"; }
   }
