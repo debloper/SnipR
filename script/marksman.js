ddb0dc329c90e8eabf5e5682b241a9438ebfb15b
Soumya Deb <debloper@gmail.com>
2012-07-04 21:44:26 +0530

    JavaScript cleansing [part 3]: optimization

--- script/marksman.js
+++ script/marksman.js
@@ -12,7 +12,6 @@ var snipr = {
 	},
 
 	processResponse:  function(info) {
-		console.log(info);
 		DOM.createNodes(snipr.target, info);
 		DOM.initMap(info.Latitude, info.Longitude);
 	}
@@ -36,26 +35,26 @@ var DOM = {
 	},
 
 	pushTimezone: function(args) {
-		offset = args.Gmtoffset;
-		if(offset<0){ offset *= (-1); }
-		Min = ((offset)-(offset%60))/60;
-		Hr = (Min-(Min%60))/60;
-		Min = Min - Hr*60;
-		if(Hr<10) {Hr = "0"+Hr};
-		if(Min<10) {Min = "0"+Min}
-		HrMin =  Hr + ":" + Min;
-		if(args.Gmtoffset<0) {HrMin = "-" + HrMin}
-		document.getElementById("timezone").innerHTML = args.TimezoneName + " [" + HrMin + "]";
+		var offset = Math.abs(args.Gmtoffset)
+		,	Min = Math.floor(offset/60)
+		,	Hr = Math.floor(Min/60);
+
+		Min = (Min - (Hr * 60));
+		if ( Hr < 10 ) { Hr = "0" + Hr; }
+		if ( args.Gmtoffset < 0 ) { Hr = "-" + Hr; }
+		if ( Min < 10 ) { Min = "0" + Min }
+
+		document.getElementById("timezone").innerHTML = args.TimezoneName + " [" + Hr + ":" + Min + "]";
 	},
 
 	initMap: function(lat, lng) {
-		var latlng = new google.maps.LatLng(lat, lng);
-		var myOptions = {
+		var latlng = new google.maps.LatLng(lat, lng)
+		,	myOptions = {
 			zoom: 10,
 			center: latlng,
 			mapTypeId: google.maps.MapTypeId.ROADMAP
 		};
-		var map = new google.maps.Map(document.getElementById("Gmap"), myOptions);
+		new google.maps.Map(document.getElementById("Gmap"), myOptions);
 	}
 };
 
@@ -66,7 +65,6 @@ var DOM = {
 	,	ipKey = "?key=4446346aef4454ce2826b0121ae23e1d29dabfe57cc8d97b678136769ab9a270"
 	,	ipQuery = "&output=json&timezone=true&callback=snipr.processResponse&ip=";
 
-	console.log(snipr.target);
 	var jsonp = document.createElement('script');
 	jsonp.src = ipApi + ipKey + ipQuery + snipr.target["IP"] +"&rand="+ Math.random();
 	document.getElementsByTagName('head')[0].appendChild(jsonp);
