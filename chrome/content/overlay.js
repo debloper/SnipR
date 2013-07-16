9872d4cfcbde238f83fdab71186cc174dca81935
Soumya Deb <debloper@gmail.com>
2013-07-16 20:54:25 +0530

    Version bump, FxAndroid related changes (GUID, UI etc.)

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -1,67 +1,70 @@
 window.addEventListener("load", function(event) { snipr.onLoad(event) }, false);
 
 var snipr = {
-  firefox: false,
-  crosshair: {},
-  host: "",
+	firefox: false,
+	crosshair: {},
+	host: "",
 
-  onLoad: function(event) {
-	this.initialized = true;
-	this.strings = document.getElementById("snipr-overlay");
-	if ( gBrowser ) {
-		snipr.firefox = true;
-		snipr.crosshair = document.getElementById("snipr-crosshair");
-		gBrowser.tabContainer.addEventListener("TabSelect",
-			function(event) { snipr.crosshair.value = "SnipR"; }, false);
-	}
-  },
-
-  resolveHost: function() {
-	  if (snipr.firefox) {
-		  return gBrowser.contentWindow.location.hostname;
-	  } else return getBrowser().currentURI.host;
-  },
-
-  renderUrl: function(host,ip,browser) {
-	  return "http://debloper.github.com/SnipR/"+browser+".htm?"+host+"#"+ip;
-  },
-
-  splitIp: function(a) {
-	var IPs = new Array();
-	while (a && a.hasMore()) { IPs.push(a.getNextAddrAsString()); }
-	return IPs[0];
-  },
-
-  label: {
-	onLookupComplete : function(a,b,c) {
-		if (b) {
-			var ip = snipr.splitIp(b);
-			snipr.crosshair.value = ip;
-		}	else snipr.crosshair.value = "No Network";
-	}
-  },
-
-  trigger: {
-	onLookupComplete : function(a,b,c) {
-		if (b) {
-			var host = snipr.host, ip = snipr.splitIp(b);
-			if (snipr.firefox) {
-				gBrowser.selectedTab = gBrowser.addTab(snipr.renderUrl(host,ip,"firefox"),
-				{referrerURI:gBrowser.currentURI, owner:gBrowser.selectedTab});
-			} else { Browser.selectedTab = Browser.addTab(snipr.renderUrl(host,ip,"fennec")); }
+	onLoad: function(event) {
+		this.initialized = true;
+		this.strings = document.getElementById("snipr-overlay");
+		if ( gBrowser ) {
+			snipr.firefox = true;
+			snipr.crosshair = document.getElementById("snipr-crosshair");
+			gBrowser.tabContainer.addEventListener("TabSelect",
+				function(event) { snipr.crosshair.value = "SnipR"; }, false);
 		}
-	}
-  },
+	},
 
-  fire: function(target) {
-	snipr.host = snipr.resolveHost();
-	var callBack = snipr[target];
-	var Cc = Components.classes, Ci = Components.interfaces;
-	var theThread = Cc["@mozilla.org/thread-manager;1"]
-		.getService(Ci.nsIThreadManager).currentThread;
-	try {
-		Cc['@mozilla.org/network/dns-service;1'].getService(Ci.nsIDNSService)
-		.asyncResolve(snipr.host, true, callBack, theThread);
-	} catch (e) { snipr.crosshair.value = "Local Page"; }
-  }
+	resolveHost: function() {
+		if (snipr.firefox) {
+			return gBrowser.contentWindow.location.hostname;
+		} else return getBrowser().currentURI.host;
+	},
+
+	renderUrl: function(host, ip) {
+		return "http://code.debs.io/snipr/marksman.htm?"+host+"#"+ip;
+	},
+
+	splitIp: function(aRecord) {
+		var IPs = new Array();
+		while (aRecord && aRecord.hasMore()) { IPs.push(aRecord.getNextAddrAsString()); }
+		return IPs[0];
+	},
+
+	label: {
+		onLookupComplete : function(aRequest, aRecord, aStatus) {
+			if (aRecord) {
+				var ip = snipr.splitIp(aRecord);
+				snipr.crosshair.value = ip;
+			}	else snipr.crosshair.value = "No Network";
+		}
+	},
+
+	trigger: {
+		onLookupComplete : function(aRequest, aRecord, aStatus) {
+			if (aRecord) {
+				var host = snipr.host, ip = snipr.splitIp(aRecord);
+				if (snipr.firefox) {
+					gBrowser.selectedTab = gBrowser.addTab(
+											snipr.renderUrl(host, ip), {
+											referrerURI:gBrowser.currentURI,
+											owner:gBrowser.selectedTab
+										});
+				} else Browser.selectedTab = Browser.addTab(snipr.renderUrl(host, ip));
+			}
+		}
+	},
+
+	fire: function(target) {
+		snipr.host = snipr.resolveHost();
+		var callBack = snipr[target];
+		var Cc = Components.classes, Ci = Components.interfaces;
+		var theThread = Cc["@mozilla.org/thread-manager;1"]
+			.getService(Ci.nsIThreadManager).currentThread;
+		try {
+			Cc['@mozilla.org/network/dns-service;1'].getService(Ci.nsIDNSService)
+			.asyncResolve(snipr.host, true, callBack, theThread);
+		} catch (e) { snipr.crosshair.value = "Local Page"; }
+	}
 };
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 SnipR
 =====
-**`Server Network IP Resolver`** : Firefox & Fennec addon to geolocate webservers.
+**`Server Network IP Resolver`** : Firefox addon to geolocate webservers.
 
 
 Description
@@ -9,9 +9,11 @@ SnipR provides the most light and minimal interface to quick-lookup the Geolocat
 
 Total timeout for hideous web-servers!
 
+P.S: the `whois` feature that comes with **v4.0.0** is experimental as of now. It's getting there - but, please don't rely on it just yet!
+
 
 Installation
 ============
-Get **SnipR** on your Mozilla browser from [addons.mozilla.org](https://addons.mozilla.org/en-US/firefox/addon/snipr/). You will need to restart the browser for the addon to take effects.
+Get **SnipR** on your Mozilla browser from [addons.mozilla.org](https://addons.mozilla.org/addon/snipr). You will need to restart the browser for the addon to take effects.
 
 Version update is automatic & the browser will show the update notification.
